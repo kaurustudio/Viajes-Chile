bed74788da923add7d7fff135385883cc95f7f87
kaurustudio <kauru.studio@gmail.com>
2022-07-15 18:22:18 -0500

    Proyecto terminando, funcionando y documentado.

---
+++ assets/js/scripts.js
@@ -0,0 +1,37 @@
+
+
+/*Script aplicado al botón enviar del formulario al final de la web*/
+
+$(document).ready (function(){
+    var botoncorreo = $("#enviar");
+     botoncorreo.on("click", function(){
+         alert("El mensaje fue enviado correctamente");
+     });
+
+
+     /*Script para cambiar el color de los títulos h2 de cada sección, quiénes somos, destacados, contacto*/
+
+
+        $("#cambiodecolorquienessomos").on('dblclick', function() {
+            $("#cambiodecolorquienessomos").css({'color':'#68CCC3'});
+            });
+
+            $("#cambiodecolordestacados").on('dblclick', function() {
+                $("#cambiodecolordestacados").css({'color':'#68CCC3'});
+                });
+
+                $("#cambiodecolorcontacto").on('dblclick', function() {
+                    $("#cambiodecolorcontacto").css({'color':'#68CCC3'});
+                    });         
+        
+               /*Script para ocultar el texto de las tarjetas ubicadas en la sección destacados*/
+
+                $(".card-title").on ("click", function(){
+                    $(".card-text").toggle ("fast", function(){
+                
+                    });
+                });
+                
+                    
+
+    });
